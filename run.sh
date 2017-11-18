c8000e90d7698e0814488d501e078971c61a0265
Bruno Cantisano <bruno.cantisano@gmail.com>
2017-11-18 11:47:59 -0200

    changed port

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker run -d --name meteor -p 9413:3000 \
+docker run -d --name meteor -p 9415:3000 \
            --env ROOT_URL=http://localhost \
            --env MONGO_URL=mongodb://admin:admin@blablabla.mlab.com:23603/meteor \
-           --env PORT=9013 \
+           --env PORT=9015 \
            -v /media/usbraid/docker/meteor:/home/meteor \
            -v /media/usbraid/docker/meteor/log:/var/log/ \
            paperinik/rpi-meteor
